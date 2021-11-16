c3b8f585c3310019054bea113533d89a2f2804f5
JieFu Zhu <1404914454@qq.com>
2021-11-16 22:25:08 +0800

    sbe_prototyping

---
+++ src/test/java/org/example/sbe_prototyping/SbePrototypingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sbe_prototyping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbePrototypingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
