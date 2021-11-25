6b28cc18c5c19801ede645e5665fde378692dba3
omarsherif <osherifo93@gmail.com>
2021-11-25 22:30:35 +0100

    added docker for camunda engine

--- source/camunda_engine/src/main/java/com/example/workflow/Application.java
+++ source/camunda_engine/src/main/java/com/example/workflow/Application.java
@@ -13,18 +13,20 @@ import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 
 
 @SpringBootApplication
 @EnableProcessApplication
-public class Application {
+public class Application  {
   public static void main(String... args) {
     SpringApplication.run(Application.class, args);
     SimulationExecutor.execute(DateTime.now().minusMonths(1).toDate(), DateTime.now().toDate());
 
   }
+ 
 
   @Autowired
   private RuntimeService runtimeService;
