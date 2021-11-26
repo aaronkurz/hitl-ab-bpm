79e230c2098366aebaa9aebe7887b477a2aff055
Aaron Kurz <aaronkurz@protonmail.com>
2021-11-26 16:37:56 +0100

    prevent backend from failing in case database service is not fully up yet

--- source/backend/create_app.py
+++ source/backend/create_app.py
@@ -1,7 +1,8 @@
-
 from flask import Flask
 from models import db
 import config
+import time
+
 
 def create_app():
     app = Flask(__name__)
@@ -9,5 +10,17 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.app_context().push()
     db.init_app(app)
-    db.create_all()
+
+    # Connect to database
+    tries = 10
+    while tries > 0:
+        try:
+            db.create_all()
+            tries = 0
+        except:
+            tries += -1
+            seconds = 10
+            print('Failed to connect to database. Waiting %i seconds and then trying again (try countdown: %s)' % (seconds, tries))
+            time.sleep(seconds)  # Wait a bit until database is loaded
+
     return app
