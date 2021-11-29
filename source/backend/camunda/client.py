a69bb368d23dee34bedf907632bcc653679d9a5f
Bennet <36090920+Ben-Ed@users.noreply.github.com>
2021-11-29 12:45:58 +0100

    Added comments + function for retrieving data

--- source/backend/camunda/client.py
+++ source/backend/camunda/client.py
@@ -23,9 +23,15 @@ class CamundaClient:
         return response
 # END TODO: remove just for demonstration
 
+
+# Status
     def status_code_successful(self,status_code: int):
         return str(status_code)[0] == '2'
 
+
+### Section: Deployment & Starting ##
+
+# Deploying a single process
     def deploy_process(self,bpmn_file: str):
 
         multipart_form_data = { 
@@ -41,21 +47,31 @@ class CamundaClient:
 
         return new_process_id
 
+
+# Deploying multiple processes
     def deploy_processes(self,bpmn_files: list):
         new_process_ids=[]
         for bpmn_file in bpmn_files:
             new_process_ids+=self.deploy_process(bpmn_file)
         return new_process_ids
 
+
+# Starting a single instance
     def start_instance(self,process_id: int):
         headers = {'Content-Type': 'application/json'}
         response = requests.post("http://localhost:8080/engine-rest/process-definition/" + str(process_id)+ "/start", headers=headers)
         assert(self.status_code_successful(response.status_code))
 
+
+# Starting multiple instances
     def start_instances(self,process_id: int,instance_count: int):
         for _ in range(instance_count):
             self.start_instance(process_id)
 
+
+### Section: Data Management ###
+
+# Deleting all data
     def delete_all_data(self,target: str):
         POSSIBLE_TARGETS = ["process-instance", "process-definition", "deployment", "decision-definition"]
         if target not in POSSIBLE_TARGETS:
@@ -74,13 +90,33 @@ class CamundaClient:
         assert(self.status_code_successful(get_response.status_code))
         assert(len(get_response.json()) == 0)   
 
-    
+
+# Cleaning Data
     def clean_process_data(self):
 
         for elem in ["process-instance", "process-definition", "deployment", "decision-definition"]:
             self.delete_all_data(elem)
 
     
+# Retrieving Data
 
+    def retrieve_data(self):
+
+        # Getting the ID, Key and Name of the process from the json file
+        for elem in proc_def_response.json():
+            processDefinitionId = elem.get('processDefinitionId')
+            processDefinitionKey = elem.get('processDefinitionKey')
+            processDefinitionName = elem.get('processDefinitionName')
+
+            params = {'processDefinitionId': str(processDefinitionId)}
+            proc_inst_response = requests.get(self.url + "/history/process-instance", params=params)
+
+            assert (status_code_successful(proc_inst_response.status_code))
+
+            # Calculating the duration of the process
+            for elem2 in proc_inst_response.json():
+                end_datetime = parser.parse(elem2.get('endTime'))
+                start_datetime = parser.parse(elem2.get('startTime'))
+                processDuration = (end_datetime - start_datetime)
 
 
