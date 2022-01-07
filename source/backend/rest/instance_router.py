8c0f6e032c3cf56ae1b6de525afaea39a7524e77
Aaron <aaronkurz@posteo.de>
2022-01-07 12:06:21 +0100

    added first version of client simulator
    yet to be manually tests

--- source/backend/rest/instance_router.py
+++ source/backend/rest/instance_router.py
@@ -1,24 +1,18 @@
-
 from flask import Blueprint, request
 from models import processes
 from camunda.client import CamundaClient
-
-
-
 
 instance_router_api = Blueprint('instance_router_api', __name__)
 
 
 @instance_router_api.route('/start-instance', methods=['GET'])
 def start_process():
-
     data = request.json
 
-    process_id=data['process_id']
+    process_id = int(request.args.get('process-id'))
+    customer_category = request.args.get('customer-category')
 
-
-    process=processes.get_process_metadata(process_id)
-
+    process = processes.get_process_metadata(process_id)
 
     # get rl env decision
 
@@ -26,22 +20,8 @@ def start_process():
 
     client = CamundaClient()
 
-    variant_a_camunda_id=process['variant_a_camunda_id']
+    variant_a_camunda_id = process['variant_a_camunda_id']
 
-    camunda_instance_id=client.start_instance(variant_a_camunda_id)
+    camunda_instance_id = client.start_instance(variant_a_camunda_id)
 
-    return camunda_instance_id
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
+    return {"camunda_instance_id": camunda_instance_id}
