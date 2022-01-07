6bc38dc76e599a25a52b85241e301402beffd7b2
Aaron <aaronkurz@posteo.de>
2022-01-07 15:52:35 +0100

    renaming and restructuring

--- source/backend/process_bandit/rl_env.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def get_decision(process_id, customer_category) -> str:
-    """ Returns either a or b
-
-    :returns 'a' or 'b'
-    """
-    return ['a', 'b'][random.randint(0, 1)]
--- source/backend/rest/instance_router.py
+++ source/backend/rest/instance_router.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, abort
 from models import processes
 from camunda.client import CamundaClient
-from process_bandit import rl_env
+from process_bandit import process_bandit
 
 instance_router_api = Blueprint('instance_router_api', __name__)
 
@@ -16,19 +16,30 @@ def start_process():
     process = processes.get_process_metadata(process_id)
 
     # get decision from process bandit
-    decision = rl_env.get_decision(process_id, customer_category)
+    if not process_bandit.is_ready_for_decision():
+        return {"instantiated": False,
+                "message": "Server not ready for instantiation of processes. Try setting a process with two variants "
+                           "and a learning/batch-policy first."}
+    decision = process_bandit.get_decision(process_id, customer_category)
 
     # instantiate according to decision
     client = CamundaClient()
-    camunda_instance_id = None
+    variant_key = None
     if decision == 'a':
-        variant_a_camunda_id = process['variant_a_camunda_id']
-        camunda_instance_id = client.start_instance(variant_a_camunda_id)
+        variant_key = 'variant_a_camunda_id'
     elif decision == 'b':
-        variant_a_camunda_id = process['variant_a_camunda_id']
-        camunda_instance_id = client.start_instance(variant_a_camunda_id)
+        variant_key = 'variant_b_camunda_id'
     else:
         abort(500, 'Unexpected decision by reinforcement learning environment')
 
+    variant_a_camunda_id = process[variant_key]
+    camunda_instance_id = client.start_instance(variant_a_camunda_id)
+    # TODO: write decision in database
+
     # return instance id
-    return {"camunda_instance_id": camunda_instance_id}
+    return {
+        "instantiated": True,
+        "camunda_instance_id": camunda_instance_id}
+
+
+# TODO: API endpoints to retrieve decision history
