b3942278bb30cdde8ea37e3c548908deba0fb965
Aaron <62746723+aaronkurz@users.noreply.github.com>
2022-01-07 16:09:36 +0100

    typo

--- source/backend/unit-tests/process_bandit_test.py
+++ source/backend/unit-tests/process_bandit_test.py
@@ -2,7 +2,7 @@ from process_bandit import process_bandit
 
 
 def test_pb_ready():
-    """ Test of process bandit returns a or b when it is ready """
+    """ Test if process bandit returns a or b when it is ready """
     assert True, process_bandit.is_ready_for_decision()
     decision = process_bandit.get_decision(5, "gov")
     assert decision == 'a' or decision == 'b'
