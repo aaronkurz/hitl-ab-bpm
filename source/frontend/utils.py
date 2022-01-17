d8719868f6e12a4d5f782c75e70e95f46cef26c1
Aaron <aaronkurz@posteo.de>
2022-01-17 15:02:28 +0100

    fix frontend manual decision button

--- source/frontend/utils.py
+++ source/frontend/utils.py
@@ -13,7 +13,7 @@ def post_manual_decision(manual_decision: str) -> bool:
     assert manual_decision in ['a', 'b']
     params = {
         "process-id": get_currently_active_process_id(),
-        "version-decision": 'a'
+        "version-decision": manual_decision
     }
     response = requests.post(BACKEND_URI + "/instance-router/manual-decision", params=params)
     return response.status_code == requests.codes.ok
