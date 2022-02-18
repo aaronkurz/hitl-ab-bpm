bd0efd86f11bd791abe0b121cd77c62b163f138d
Aaron <aaronkurz@posteo.de>
2022-02-18 17:16:45 +0100

    change workflows and fix some pylint issues

--- source/backend/camunda/client.py
+++ source/backend/camunda/client.py
@@ -1,146 +1,43 @@
+""" Used for communication with Camunda Engine """
 import requests
-
 import config
 
 
 class CamundaClient:
+    """ Class used for communication with camunda engine """
     def __init__(self, url=config.CAMUNDA_ENGINE_URI):
         self.url = url
 
-    # TODO: remove just for demonstration
-    def get_task(self, task):
-        response = requests.get(self.url + f"/task/{task}")
-        return response
-
-    def create_task(self, data):
-        response = requests.post(self.url + "/task/create", json=data)
-        return response
-
-    def update_task(self, task, data):
-        response = requests.put(self.url + f"/task/{task}", json=data)
-        return response
-
-    def list_tasks(self):
-        response = requests.get(self.url + "/task")
-        return response
-
-    # END TODO: remove just for demonstration
-
     # Status
-    def status_code_successful(self, status_code: int):
+    @classmethod
+    def status_code_successful(cls, status_code: int):
+        """ Assert whether an HTTP status code is successful """
         return str(status_code)[0] == '2'
 
     ### Section: Deployment & Starting ##
 
     # Deploying a single process
     def deploy_process(self, path_bpmn_file: str):
-        multipart_form_data = {
-            'deployment-name': (None, 'store'),
-            'data': (path_bpmn_file.split("/")[len(path_bpmn_file.split("/")) - 1], open(path_bpmn_file, 'r')),
-        }
-        response = requests.post(self.url + "/deployment/create", files=multipart_form_data)
-        assert (self.status_code_successful(response.status_code))
+        """ Send a process to Camunda Engine and retrieve process id in Camunda Engine """
+        with open(path_bpmn_file, 'r', encoding="UTF-8") as bpmn_file:
+            multipart_form_data = {
+                'deployment-name': (None, 'store'),
+                'data': (path_bpmn_file.split("/")[len(path_bpmn_file.split("/")) - 1], bpmn_file),
+            }
+            response = requests.post(self.url + "/deployment/create", files=multipart_form_data)
+            assert self.status_code_successful(response.status_code)
 
-        assert len(response.json().get('deployedProcessDefinitions')) == 1
-        new_process_id = None
-        for elem in response.json().get('deployedProcessDefinitions'):
-            new_process_id = elem
+            assert len(response.json().get('deployedProcessDefinitions')) == 1
+            new_process_id = None
+            for elem in response.json().get('deployedProcessDefinitions'):
+                new_process_id = elem
 
-        return new_process_id
-
-    # Deploying multiple processes
-    def deploy_processes(self, bpmn_files: list):
-        new_process_ids = []
-        for bpmn_file in bpmn_files:
-            new_process_ids.append(self.deploy_process(bpmn_file))
-        return new_process_ids
+            return new_process_id
 
     # Starting a single instance
     def start_instance(self, process_id: int):
+        """ Start an instance with a certain camunda process id """
         headers = {'Content-Type': 'application/json'}
         response = requests.post(self.url + "/process-definition/" + str(process_id) + "/start", headers=headers)
         assert self.status_code_successful(response.status_code)
         return response.json().get('id')
-
-    # Starting multiple instances
-    def start_instances(self, process_id: int, instance_count: int):
-        all_instances_successful = True
-        for _ in range(instance_count):
-            all_instances_successful = all_instances_successful and self.start_instance(process_id)
-
-        return all_instances_successful
-
-    # 2
-    def terminate_instance2(self, process_id: int):
-
-        delete = requests.post(self.url + str(process_id) + "/terminate")
-
-        return delete
-
-    ### Section: Data Management ###
-
-    # Deleting all data
-    def delete_all_data(self, target: str):
-        POSSIBLE_TARGETS = ["process-instance", "process-definition", "deployment", "decision-definition"]
-        if target not in POSSIBLE_TARGETS:
-            raise RuntimeError(str(target) + "not a valid data deletion target")
-
-        if target == 'process-definition':
-            self.delete_all_data(target="process-instance")
-
-        get_response = requests.get(self.url + "/" + target)
-        assert (self.status_code_successful(get_response.status_code))
-
-        for elem in get_response.json():
-            current_id = elem.get('id')
-            del_response = requests.delete(self.url + "/" + target + "/" + str(current_id))
-            assert (self.status_code_successful(del_response.status_code))
-
-        get_response = requests.get(self.url + "/" + target)
-        assert (self.status_code_successful(get_response.status_code))
-        assert (len(get_response.json()) == 0)
-
-    # Cleaning Data
-    def clean_process_data(self):
-
-        for elem in ["process-instance", "process-definition", "deployment", "decision-definition"]:
-            self.delete_all_data(elem)
-
-    # Retrieving Data
-
-    def retrieve_data(self):
-
-        proc_def_response = requests.get(
-            self.url + "/history/process-definition/cleanable-process-instance-report").json()
-
-        proc_def_history = proc_def_response
-
-        # Getting the ID, Key and Name of the process from the json file
-        for elem in proc_def_history:
-            process_definition_id = elem.get('processDefinitionId')
-
-            params = {'processDefinitionId': str(process_definition_id)}
-            proc_inst_response = requests.get(self.url + "/history/process-instance", params=params)
-
-            assert (self.status_code_successful(proc_inst_response.status_code))
-
-            associated_instances = []
-            # Calculating the duration of the process
-            for elem2 in proc_inst_response.json():
-                associated_instances.append(elem2)
-
-            elem['associated_instances'] = associated_instances
-
-        return proc_def_history
-
-
-"""
-#lines 107-109 could be deleted. 
-#Or "processDefinitionId" = processDefinitionId
-        data = {
-            "processDefinitionId" : elem.get('processDefinitionId'),
-            "processDefinitionKey" : elem.get('processDefinitionKey'),
-            "processDefinitionName" : elem.get('processDefinitionName')
-        }
-        return data
-"""
--- source/backend/models/batch_policy.py
+++ source/backend/models/batch_policy.py
@@ -1,13 +1,13 @@
+""" SQLAlchemy models and helper functions regarding Batch Policy """
 from datetime import datetime
-
-from sqlalchemy import desc, asc
-
+from sqlalchemy import desc
 from models import db
 from models.process_instance import ProcessInstance
 from models.utils import CASCADING_DELETE
 
 
 class BatchPolicy(db.Model):
+    """ SQLAlchemy Model for BatchPolicy """
     __tablename__ = 'batch_policy'
     id = db.Column(db.Integer, primary_key=True)
     batch_size = db.Column(db.Integer, nullable=False)
@@ -22,6 +22,7 @@ class BatchPolicy(db.Model):
 
 
 class ExecutionStrategyBaPol(db.Model):
+    """ SQLAlchemy model for ExecutionStrategyBaPol """
     __tablename__ = "execution_strategy_bapol"
     batch_policy_id = db.Column(db.Integer, db.ForeignKey('batch_policy.id'), primary_key=True)
     customer_category = db.Column(db.String(100), primary_key=True)
@@ -30,10 +31,20 @@ class ExecutionStrategyBaPol(db.Model):
 
 
 def get_latest_bapol_entry(process_id: int):
+    """
+    Get latest Batch Policy table entry
+    :param process_id: Process id in our backend
+    :return: Instance of BatchPolicy
+    """
     return BatchPolicy.query.filter(BatchPolicy.process_id == process_id).order_by(desc(BatchPolicy.id)).first()
 
 
 def append_process_instance_to_bapol(process_id: int, process_instance):
+    """
+    Append a process instance to a bapol
+    :param process_id: id of process in our backend
+    :param process_instance: instance of models.process.ProcessInstance
+    """
     relevant_bapol = get_latest_bapol_entry(process_id)
     relevant_bapol.process_instances.append(process_instance)
 
@@ -68,22 +79,35 @@ def get_current_bapol_data_active_process():
 
 
 def is_latest_batch_done(process_id: int) -> bool:
+    """
+    Check whether the latest batch policy is done
+    :param process_id: Process id in our backend
+    :return: True or False
+    """
     latest_bapol = BatchPolicy.query.order_by(BatchPolicy.time_added.desc()) \
         .filter(BatchPolicy.process_id == process_id).first()
     if latest_bapol.batch_size == latest_bapol.instances.count():
         return True
-    elif latest_bapol.batch_size < latest_bapol.instances.count():
+    if latest_bapol.batch_size < latest_bapol.instances.count():
         return False
-    else:
-        raise RuntimeError("Batch Policy size has been exceeded. Illegal state.")
+    raise RuntimeError("Batch Policy size has been exceeded. Illegal state.")
 
 
 def is_latest_batch_active_process_done() -> bool:
+    """
+    Check whether the latest batch policy of the currently active process/experiment is done
+    :return: True or False
+    """
     from models.process import get_active_process_metadata
     return is_latest_batch_done(get_active_process_metadata().get('id'))
 
 
 def get_batch_size_sum(process_id: int) -> int:
+    """
+    Get sum of sizes of all batches of a process
+    :param process_id: Process id
+    :return: Sum of batch sizes for a process
+    """
     relevant_batch_policies = BatchPolicy.query.filter(BatchPolicy.process_id == process_id)
     batch_size_counter = 0
     for bapol in relevant_batch_policies:
@@ -92,6 +116,11 @@ def get_batch_size_sum(process_id: int) -> int:
 
 
 def get_number_finished_bapols(process_id) -> int:
+    """
+    Get number of finished batch policy proposals for a process
+    :param process_id: Process id
+    :return: Number of finished batch policy proposals
+    """
     relevant_bapols = BatchPolicy.query.filter(BatchPolicy.process_id == process_id)
     counter = 0
     for bapol in relevant_bapols:
@@ -102,6 +131,11 @@ def get_number_finished_bapols(process_id) -> int:
 
 
 def get_average_batch_size(process_id) -> float:
+    """
+    Get average batch size of a certain process
+    :param process_id: Process id
+    :return: Average batch size
+    """
     bapol_count = BatchPolicy.query.filter(BatchPolicy.process_id == process_id).count()
     bapol_size_sum = get_batch_size_sum(process_id)
     return bapol_size_sum / bapol_count
