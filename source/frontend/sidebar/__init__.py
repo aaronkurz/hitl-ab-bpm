fd767ff41696fd524719c791afac75ce2e3898da
Aaron <aaronkurz@posteo.de>
2022-03-06 09:55:51 +0100

    extend sidebar

--- source/frontend/sidebar/__init__.py
+++ source/frontend/sidebar/__init__.py
@@ -3,4 +3,12 @@ import streamlit as st
 def build_bar():
     st.sidebar.image("resources/images/favicon.png")
     st.sidebar.title("HITL-AB-BPM")
-    st.sidebar.write("## Continuous, controllable process improvement")
+    st.sidebar.write("### Continuous, rapid and controllable process improvement")
+    st.sidebar.write("""
+    - Continuous: AB Testing methodology
+    - Rapid: Supported by reinforcement learning
+    - Controllable: Expert oversight (Human-in-the-Loop)
+    
+    For reporting bugs, feedback and more info, please refer
+     to the [Github repository](https://github.com/Ultimate-Storm/sbe_prototyping).
+    """)
